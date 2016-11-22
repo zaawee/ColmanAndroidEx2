8c092d36bb8359f06d033422271e401bd2e7a800
zaawee <zaawee@gmail.com>
2016-11-22 22:09:42 +0200

    First commit
    Only layout and some code

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import static android.R.id.button1;
+
+public class MainActivity extends Activity {
+
+    private int[][] grid = new int[3][3];
+    private boolean isXTurn = true;
+
+    private class ClickListener implements View.OnClickListener{
+        public void onClick(View view){
+            ImageButton button = (ImageButton)view;
+            if(isXTurn){
+                ((ImageButton) view).setImageResource(R.drawable.x);
+            }
+        }
+    }
+
+    final ClickListener singleListener = new ClickListener();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton button1 = (ImageButton)findViewById(R.id.square1);
+        button1.setOnClickListener(singleListener);
+        button1.setTag(1,1);
+        ImageButton button2 = (ImageButton)findViewById(R.id.square2);
+        button2.setOnClickListener(singleListener);
+        ImageButton button3 = (ImageButton)findViewById(R.id.square3);
+        button3.setOnClickListener(singleListener);
+        ImageButton button4 = (ImageButton)findViewById(R.id.square4);
+        button4.setOnClickListener(singleListener);
+        ImageButton button5 = (ImageButton)findViewById(R.id.square5);
+        button5.setOnClickListener(singleListener);
+        ImageButton button6 = (ImageButton)findViewById(R.id.square6);
+        button6.setOnClickListener(singleListener);
+        ImageButton button7 = (ImageButton)findViewById(R.id.square7);
+        button7.setOnClickListener(singleListener);
+        ImageButton button8 = (ImageButton)findViewById(R.id.square8);
+        button8.setOnClickListener(singleListener);
+        ImageButton button9 = (ImageButton)findViewById(R.id.square9);
+        button9.setOnClickListener(singleListener);
+    }
+
+}
