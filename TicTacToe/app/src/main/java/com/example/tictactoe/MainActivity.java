036f5487295c59b8598d030f52bc035fcf8324b9
Michael Golbraikh <mgolbraikh@gmail.com>
2016-11-23 17:00:35 +0200

    Merge remote-tracking branch 'refs/remotes/origin/MichaelDev'
    # Conflicts:
    #	TicTacToe/app/build.gradle
    #	TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.tictactoe;
 
 import android.app.Activity;
-<<<<<<< HEAD
-=======
 import android.graphics.Point;
-import android.media.Image;
-import android.net.Uri;
->>>>>>> refs/remotes/origin/MichaelDev
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -18,56 +13,7 @@ public class MainActivity extends Activity {
 
     private int[][] grid = new int[3][3];
     private boolean isXTurn = true;
-<<<<<<< HEAD
-
-    private class ClickListener implements View.OnClickListener{
-        public void onClick(View view){
-            ImageButton button = (ImageButton)view;
-            if(isXTurn){
-                ((ImageButton) view).setImageResource(R.drawable.x);
-=======
     private TicTacToeGrid _game;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 
     private class ClickListener implements View.OnClickListener {
         public void onClick(View view) {
@@ -115,7 +61,7 @@ public class MainActivity extends Activity {
 
                 // Pass to opponent
                 isXTurn = !isXTurn;
->>>>>>> refs/remotes/origin/MichaelDev
+
             }
         }
     }
@@ -127,15 +73,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
+
         ImageButton button1 = (ImageButton)findViewById(R.id.square1);
-=======
+
         // Create board game
         this._game = new TicTacToeGrid();
 
         // Init the buttons with places
-        ImageButton button1 = (ImageButton) findViewById(R.id.square1);
->>>>>>> refs/remotes/origin/MichaelDev
+
         button1.setOnClickListener(singleListener);
         button1.setTag(1,1);
         ImageButton button2 = (ImageButton)findViewById(R.id.square2);
